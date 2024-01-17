f9841b037484a3f1c84b16573a24898e9e631119
Nitesh <05niteshraj@gmail.com>
2024-01-18 02:21:15 +0530

    Added some routes;

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,12 @@
 import express from "express";
+import {
+  getWebSocketUrl,
+  storeMessage,
+} from "../controllers/http.controller.js";
 
 const routes = express.Router();
+
+routes.route("/chat").post(storeMessage);
+routes.route("/websocket-url").get(getWebSocketUrl);
 
 export default routes;
